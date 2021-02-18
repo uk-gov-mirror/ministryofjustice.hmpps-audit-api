d59be49e2be1814bd96b3076a4584c924c0f96c8
Andy Marke <andy.marke@digital.justice.gov.uk>
2021-02-18 14:04:26 +0000

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.1.0"
+  kotlin("plugin.spring") version "1.4.30"
+}
+
+configurations {
+  testImplementation { exclude(group = "org.junit.vintage") }
+}
+
+dependencies {
+  implementation("org.springframework.boot:spring-boot-starter-webflux")
+}
